4b8eeff74546c4379afcfc51af4d868de60aecd6
Adrian Wälchli <adrian.waelchli@inf.unibe.ch>
2020-03-01 14:24:25 +0100

    fix package install

--- mlutils/__init__.py
+++
@@ -1,7 +0,0 @@
-import mlutils.dataset
-import mlutils.geometry
-import mlutils.graphics
-import mlutils.layer
-import mlutils.logging
-import mlutils.loss
-import mlutils.optical_flow
--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='mlutils',
-    version='0.0.1',
+    version='0.0.3',
     author='Adrian Wälchli',
     author_email='adrian.waelchli@inf.unibe.ch',
-    packages=['mlutils'],
     scripts=[],
-    url='',
+    url='https://github.com/awaelchli/mlutils.git',
     license='LICENSE.txt',
     description='Machine Learning Utilities',
     long_description=open('README.md').read(),
+    packages=find_packages(exclude=['tests']),
     install_requires=[
         'torch>=1.2.0',
         'torchvision>=0.4.0'
