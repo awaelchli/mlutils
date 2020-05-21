a2dadecd99257b2285850cee6bf9eccd8b020d77
Adrian Wälchli <adrian.waelchli@inf.unibe.ch>
2020-05-21 13:34:56 +0200

    update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='mlutils',
-    version='0.0.6',
+    version='0.0.7',
     author='Adrian Wälchli',
     author_email='adrian.waelchli@inf.unibe.ch',
     scripts=[],
